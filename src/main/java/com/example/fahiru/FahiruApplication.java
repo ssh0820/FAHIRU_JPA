1963cf81994e994b201bedf3995e44ce9fb5fb3e
ssh0820 <53414519+ssh0820@users.noreply.github.com>
2022-07-12 21:27:05 +0900

    초기 세팅
    초기 세팅

---
+++ src/main/java/com/example/fahiru/FahiruApplication.java
@@ -0,0 +1,13 @@
+package com.example.fahiru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahiruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahiruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fahiru/FahiruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fahiru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahiruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
