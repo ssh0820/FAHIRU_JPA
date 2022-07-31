aac0dda48a24158868eb619b22a6ca24c3544b7d
seoseunghyeon <tmdgustj@gmail.com>
2022-07-31 22:24:25 +0800

    검색 bean 추가

---
+++ src/main/java/project/fahiru/dto/FahesSearch.java
@@ -0,0 +1,13 @@
+package project.fahiru.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class FahesSearch {
+
+    private String name;
+    private String explanation;
+
+}
