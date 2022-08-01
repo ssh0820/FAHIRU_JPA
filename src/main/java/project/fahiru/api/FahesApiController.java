180ae75a9cfac581114c38500ce765e5f0411c11
seoseunghyeon <tmdgustj@gmail.com>
2022-08-01 22:54:37 +0800

    del

--- src/main/java/project/fahiru/api/FahesApiController.java
+++ src/main/java/project/fahiru/api/FahesApiController.java
@@ -2,7 +2,9 @@ package project.fahiru.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.fahiru.domain.Fahes;
@@ -22,6 +24,12 @@ public class FahesApiController {
         List<Fahes> fahesList = fahesRepository.findAll();
         model.addAttribute("fahesList", fahesList);
         return "fahes/fahesList";
+    }
+
+    @DeleteMapping("/api/fahes/{fahNo}")
+    public String deleteFah(@PathVariable Long fahNo){
+        fahesRepository.deleteById(fahNo);
+        return "redirect:/fahesList";
     }
 
 }
