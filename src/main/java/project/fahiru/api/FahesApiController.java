b3d1ae062bcb7818767d624359c7b13c46881f67
seoseunghyeon <tmdgustj@gmail.com>
2022-08-02 09:39:19 +0800

    mapping 추가

--- src/main/java/project/fahiru/api/FahesApiController.java
+++ src/main/java/project/fahiru/api/FahesApiController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.fahiru.domain.Fahes;
@@ -24,6 +25,12 @@ public class FahesApiController {
         List<Fahes> fahesList = fahesRepository.findAll();
         model.addAttribute("fahesList", fahesList);
         return "fahes/fahesList";
+    }
+
+    @PostMapping("/api/fahes")
+    public String insertFah(Fahes fahes){
+        fahesRepository.save(fahes);
+        return "redirect:/fahesList";
     }
 
     @DeleteMapping("/api/fahes/{fahNo}")
