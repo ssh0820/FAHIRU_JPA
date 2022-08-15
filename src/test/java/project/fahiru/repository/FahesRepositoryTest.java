9d3d729cfea37d45cbc4fa65fd97969c966093e7
seoseunghyeon <tmdgustj@gmail.com>
2022-08-15 17:40:14 +0800

    test 코드 시작

---
+++ src/test/java/project/fahiru/repository/FahesRepositoryTest.java
@@ -0,0 +1,14 @@
+package project.fahiru.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+class FahesRepositoryTest {
+
+    EntityManager em;
+
+    FahesRepository fahesRepository;
+
+}
