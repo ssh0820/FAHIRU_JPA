f46e4922ef5fc0448c9f0825eeee56da1069930c
seoseunghyeon <tmdgustj@gmail.com>
2022-08-23 22:05:39 +0800

    api 수정

--- src/main/java/project/fahiru/repository/TrendyFoodCookRepository.java
+++ src/main/java/project/fahiru/repository/TrendyFoodCookRepository.java
@@ -1,9 +1,48 @@
 package project.fahiru.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import project.fahiru.domain.QTrendyFoodCook;
 import project.fahiru.domain.TrendyFoodCook;
+import project.fahiru.dto.TrendySearch;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
-public interface TrendyFoodCookRepository extends JpaRepository<TrendyFoodCook, Long> {
+@RequiredArgsConstructor
+public class TrendyFoodCookRepository {
+
+    private final EntityManager em;
+
+    private JPAQueryFactory factory;
+
+    public void save(TrendyFoodCook foodCook){
+        em.persist(foodCook);
+    }
+
+    public TrendyFoodCook findOne(Long cookNo){
+        return em.find(TrendyFoodCook.class,cookNo);
+    }
+
+    public List<TrendyFoodCook> findAll (TrendySearch trendySearch){
+
+        factory = new JPAQueryFactory(em);
+        QTrendyFoodCook foodCook = QTrendyFoodCook.trendyFoodCook;
+
+        List<TrendyFoodCook> trendyList = factory.selectFrom(foodCook)
+                .where(
+                    foodCook.name.eq(trendySearch.getName())
+                        , foodCook.score.eq(trendySearch.getScore())
+                )
+                .orderBy(foodCook.no.desc()).fetch();
+
+        return trendyList;
+    }
+
+
+
+
 }
