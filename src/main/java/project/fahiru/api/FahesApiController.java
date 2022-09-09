336cadc6855c7c77a66bec8f0fa8756ecfd18538
seoseunghyeon <tmdgustj@gmail.com>
2022-09-09 15:05:59 +0800

    api 수정

--- src/main/java/project/fahiru/api/FahesApiController.java
+++ src/main/java/project/fahiru/api/FahesApiController.java
@@ -1,20 +1,14 @@
 package project.fahiru.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-import project.fahiru.domain.Fahes;
 import project.fahiru.dto.FahesResponseDto;
 import project.fahiru.dto.FahesSaveDto;
 import project.fahiru.dto.FahesSearch;
 import project.fahiru.dto.FahesUpdateDto;
-import project.fahiru.repository.FahesRepository;
 import project.fahiru.service.FahesService;
 
 import java.util.List;
--- src/test/java/project/fahiru/repository/FahesRepositoryTest.java
+++ src/test/java/project/fahiru/repository/FahesRepositoryTest.java
@@ -1,8 +1,13 @@
 package project.fahiru.repository;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import project.fahiru.domain.Fahes;
+import project.fahiru.dto.FahesSearch;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @SpringBootTest
 class FahesRepositoryTest {
@@ -10,5 +15,29 @@ class FahesRepositoryTest {
     EntityManager em;
 
     FahesRepository fahesRepository;
+
+    @Test
+    @Transactional
+    public void save(){
+
+        Fahes fahes = new Fahes("현이","등록");
+
+        fahesRepository.save(fahes);
+
+        em.flush();
+        em.clear();
+
+        FahesSearch fahesSearch = new FahesSearch("현이","등록");
+
+        List<Fahes> fahesList = fahesRepository.findAll(fahesSearch);
+
+        if (fahesList.isEmpty()){
+
+        }else{
+
+        }
+
+
+    }
 
 }
